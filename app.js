9a67bc95167f7b4dfe6e2e79fc3a31e9ff6b79f2
Rayyan Hussain <rayyan.hussain4@t-mobile.com>
2021-07-28 19:59:35 -0700

    Added API code.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -7,6 +8,33 @@ const app = express();
 app.get("/health", (req, res) => {
   res.status(200).send("Don't panic.");
 });
+
+let allBooks = {"Books":[]};
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/api/books", (req, res) => {
+  allBooks.Books.sort();
+  res.status(200).send(allBooks);
+});
+
+app.post('/api/books', (req, res) => {
+  let newBook = (req.body);
+  console.log(newBook);
+  allBooks.Books.push(newBook);
+
+  res.send(newBook);
+});
+
+app.delete('/api/books', function (req, res) {
+  for(let i in allBooks){
+    allBooks[i].pop;
+  }
+  res.status(204);
+})
+
+
 
 
 
